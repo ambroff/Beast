3d33ea3d5f8a0db2e8204a514b33a222d3a7ea75
Vinnie Falco <vinnie.falco@gmail.com>
2017-07-20 14:02:32 -0700

    [FOLD] add repo link to headers

--- include/beast.hpp
+++ include/beast.hpp
@@ -4,9 +4,11 @@
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
+// Official repository: https://github.com/boostorg/beast
+//
 
-#ifndef BEAST_HPP
-#define BEAST_HPP
+#ifndef BOOST_BEAST_HPP
+#define BOOST_BEAST_HPP
 
 #include <beast/config.hpp>
 
--- include/beast/zlib/detail/deflate_stream.hpp
+++ include/beast/zlib/detail/deflate_stream.hpp
@@ -4,6 +4,8 @@
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
+// Official repository: https://github.com/boostorg/beast
+//
 // This is a derivative work based on Zlib, copyright below:
 /*
     Copyright (C) 1995-2013 Jean-loup Gailly and Mark Adler
